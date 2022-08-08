baefa777c5abc3482c9f480c8b124539cb14d530
Nadezhda <n.d.niki2012@gmail.com>
2022-08-09 00:14:49 +0300

    fix index.js and calc-game.js

--- src/games/calc-game.js
+++ src/games/calc-game.js
@@ -1,54 +1,57 @@
 import readlineSync from 'readline-sync';
-import { userGreeting, randomNumber, endOfCycle } from '../index.js';
+import { greeting, task, randomNumber } from '../index.js';
 
 const calculator = () => {
-    const greeting = userGreeting();
-    console.log(greeting);
-    console.log('What is the result of the expression?');
+  greeting();
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  task('What is the result of the expression?');
 
-    let resultOfMathExpression;
-    const mathExpression = () => {
-        const operators = ['+', '-', '*'];
-        const indexOfOperators = [0, 1, 2];
-        const randomIndex = Math.floor(Math.random() * indexOfOperators.length);
-        const randomOperator = operators[randomIndex];
+  let resultOfMathExpression;
+  const mathExpression = () => {
+    const operators = ['+', '-', '*'];
+    const indexOfOperators = [0, 1, 2];
+    const randomIndex = Math.floor(Math.random() * indexOfOperators.length);
+    const randomOperator = operators[randomIndex];
 
-        const number1 = randomNumber();
-        const number2 = randomNumber();
+    const number1 = randomNumber();
+    const number2 = randomNumber();
 
-        switch (randomOperator) {
-            case '+':
-                resultOfMathExpression = number1 + number2;
-                return `${number1} + ${number2}`;
-                break;
-            case '-':
-                resultOfMathExpression = number1 - number2;
-                return `${number1} - ${number2}`;
-                break;
-            default:
-                resultOfMathExpression = number1 * number2;
-                return `${number1} * ${number2}`;
-        }
-    };
-
-    let result;
-    for (let i = 1; i <= 3; i += 1) {
-        console.log(`Question: ${mathExpression()}`);
-        const answerOfUser = readlineSync.question('Your answer: ');
-
-        const forCompare = resultOfMathExpression.toString();
-
-        if (forCompare === answerOfUser) {
-        result = 'Correct!';
-        console.log(result);
-        } else {
-            result = `"${answerOfUser}" is wrong answer ;(. Correct answer was "${forCompare}".`;
-            console.log(result);
-            break;
-        }
+    switch (randomOperator) {
+      case '+':
+        resultOfMathExpression = number1 + number2;
+        return `${number1} + ${number2}`;
+      case '-':
+        resultOfMathExpression = number1 - number2;
+        return `${number1} - ${number2}`;
+      default:
+        resultOfMathExpression = number1 * number2;
+        return `${number1} * ${number2}`;
     }
+  };
 
-    endOfCycle();
+  let result;
+  for (let i = 1; i <= 3; i += 1) {
+    console.log(`Question: ${mathExpression()}`);
+    const answerOfUser = readlineSync.question('Your answer: ');
+
+    const forCompare = resultOfMathExpression.toString();
+
+    if (forCompare === answerOfUser) {
+      result = 'Correct!';
+      console.log(result);
+    } else {
+      result = `"${answerOfUser}" is wrong answer ;(. Correct answer was "${forCompare}".`;
+      console.log(result);
+      break;
+    }
+  }
+
+  if (result === 'Correct!') {
+    console.log(`Congratulations, ${userName}!`);
+  } else {
+    console.log(`Let's try again, ${userName}!`);
+  }
 };
 
 export default calculator;
--- src/index.js
+++ src/index.js
@@ -1,27 +1,11 @@
-import readlineSync from 'readline-sync';
+const greeting = () => console.log('Welcome to the Brain Games!');
 
-const userGreeting = () => {
-    console.log('Welcome to the Brain Games!');
-    const userName = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${userName}!`);
-};
+const task = (str) => console.log(str);
 
 const randomNumber = () => {
-    const number = (min, max) => {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    };
+  const number = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
-    return number(1, 100);
+  return number(1, 100);
 };
 
-let result;
-const endOfCycle = () => {
-    if (result === 'Correct!') {
-        console.log(`Congratulations, ${userName}!`);
-    } else {
-        console.log(`Let's try again, ${userName}!`);
-    }
-}
-
-
-export { userGreeting, randomNumber, endOfCycle };
+export { greeting, task, randomNumber };
