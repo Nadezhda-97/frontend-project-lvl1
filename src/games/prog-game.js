b8f25149a6fcfbce50f5c7e38e7fb5a72af41483
Nadezhda <n.d.niki2012@gmail.com>
2022-08-12 22:11:16 +0300

    update index.js and prog-game.js

--- src/games/prog-game.js
+++ src/games/prog-game.js
@@ -1,2 +1,66 @@
 import readlineSync from 'readline-sync';
-import { greeting, task, randomNumber } from '../index.js';
+import {
+  greeting, task, randomNumber, randElementOfArray,
+} from '../index.js';
+
+const prog = () => {
+  greeting();
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  task('What number is missing in the progression?');
+
+  let answer;
+  const mathProgression = () => {
+    const lengthOfMathProg = [5, 6, 7, 8, 9, 10];
+    const randomLengthOfMathProg = randElementOfArray(lengthOfMathProg);
+
+    const randHiddenNumber = (number) => {
+      const num = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+      return num(0, number);
+    };
+    const hiddenNumber = randHiddenNumber(randomLengthOfMathProg);
+
+    let firstNumber = randomNumber();
+
+    const nElements = [2, 3, 5];
+    const randomNElement = randElementOfArray(nElements);
+
+    const resultOfMathProg = [];
+    for (let i = 0; i < randomLengthOfMathProg; i += 1) {
+      if (i !== hiddenNumber) {
+        firstNumber += randomNElement;
+        resultOfMathProg[i] = firstNumber;
+      } else {
+        firstNumber += randomNElement;
+        resultOfMathProg[i] = '..';
+        answer = firstNumber;
+      }
+    }
+    return resultOfMathProg;
+  };
+
+  let result;
+  for (let i = 1; i <= 3; i += 1) {
+    console.log(`Question: ${mathProgression().join(' ')}`);
+    const answerOfUser = readlineSync.question('Your answer: ');
+
+    const forCompare = answer.toString();
+
+    if (forCompare === answerOfUser) {
+      result = 'Correct!';
+      console.log(result);
+    } else {
+      result = `"${answerOfUser}" is wrong answer ;(. Correct answer was "${forCompare}".`;
+      console.log(result);
+      break;
+    }
+  }
+
+  if (result === 'Correct!') {
+    console.log(`Congratulations, ${userName}!`);
+  } else {
+    console.log(`Let's try again, ${userName}!`);
+  }
+};
+
+export default prog;
--- src/index.js
+++ src/index.js
@@ -4,8 +4,14 @@ const task = (str) => console.log(str);
 
 const randomNumber = () => {
   const number = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
-
   return number(1, 100);
 };
 
-export { greeting, task, randomNumber };
+const randElementOfArray = (array) => {
+  const randIndex = Math.floor(Math.random() * array.length);
+  return array[randIndex];
+};
+
+export {
+  greeting, task, randomNumber, randElementOfArray,
+};
